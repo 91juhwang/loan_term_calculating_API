63874db260a1ccbae330976c624258a7a3c20dcc
91juhwang <91juhwang@gmail.com>
2018-08-30 18:05:22 -0400

    Add user_spec

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+  let(:user) { create(:user) }
+  let(:headers) { valid_headers.except('Authorization') }
+  let(:valid_attributes) do
+    attributes_for(:user, password_confirmation: user.password)
+  end
+
+  describe 'POST /signup' do
+    context 'when request is valid' do
+      before { post '/signup', params: valid_attributes.to_json, headers: headers }
+
+      it 'creates a new user' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns an authentication token' do
+        expect(JSON.parse(response.body)['auth_token']).not_to be_nil
+      end
+    end
+
+    context 'when invalid request' do
+      before { post '/signup', params: {}, headers: headers }
+
+      it 'does not create a new user' do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+end
